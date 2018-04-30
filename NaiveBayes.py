863d464ebe16f936d70ccf02ee730eae03d44d7a
Tagptroll1 <thomas@galehuset.org>
2018-04-30 21:31:16 +0200

    i mek stuff

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,21 +1,38 @@
+import math
+import os
+import numpy as np
+import json
+
 import tkinter
 from tkinter import filedialog
-import numpy as np
-import NaiveBayesFunctions as nb
 from collections import Counter
-import math
+
+import NaiveBayesFunctions as nb
 from interface_functions import print_inline
+
+__location__ = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
+
 
 class NaiveBayes:
     def __init__(self):
         self.posTestReviewsList = None
         self.negTestReviewsList = None
-
         self.probs = None
         self.zeroV = None
         self.emptyPos = None
         self.emptyNeg = None
 
+        print("choose your 'test' folder")
+        tkinter.Tk().withdraw()
+        self.testDirPath = filedialog.askdirectory()
+
+    def set_review_list(self):
+        if self.posTestReviewsList is not None and self.negTestReviewsList is not None:
+            return
+        
+        self.posTestReviewsList, self.negTestReviewsList = nb.createArrayList(
+            self.testDirPath)
 
     def loadData(self):
         """
@@ -27,12 +44,8 @@ class NaiveBayes:
         print("choose your 'train' folder")
         tkinter.Tk().withdraw()
         folderpath = filedialog.askdirectory()
-        print("choose your 'test' folder")
-        tkinter.Tk().withdraw()
-        testDirPath = filedialog.askdirectory()
-        # lager en liste av de positive og en av de negative
-    # posList = nb.createArrayList(positiveData, pos)
-    # negList = nb.createArrayList(negativeData, neg)
+
+
         posList, negList = nb.createArrayList(folderpath)
         print("made the lists")
         # lager dictionaries med antall reviews disse ordene forekommer i (ut av de positive/negative)
@@ -41,7 +54,7 @@ class NaiveBayes:
         allWords = Counter(negWordsDict) + Counter(posWordsDict)
         print("made the dicts")
 
-        self.posTestReviewsList, self.negTestReviewsList = nb.createArrayList(testDirPath)
+        self.set_review_list()
         print("loading done, type 'help' for helpfull commands")
 
         pListLeng = len(posList)
@@ -49,12 +62,42 @@ class NaiveBayes:
         self.probs, self.zeroV, self.emptyPos, self.emptyNeg = nb.preProb(posWordsDict, pListLeng,
                                                       negWordsDict, nListLeng, allWords)
 
+    def saveData(self):
+        if (self.probs is None or self.zeroV is None
+                or self.emptyPos is None or self.emptyNeg is None):
+            print("Generate or import model before saving model!")
+            return
+            
+        values = {"probs": self.probs,
+                  "zeroV": self.zeroV,
+                  "emptyPos": self.emptyPos,
+                  "emptyNeg": self.emptyNeg}
+
+        with open(os.path.join(__location__, "preset_model.json"), "w") as outfile:
+            json.dump(values, outfile)
+
+    def load_data_from_file(self):
+        self.set_review_list()
+        with open(os.path.join(__location__, "preset_model.json"), "r") as data:
+            j_data = json.load(data)
+
+            self.probs = j_data.get("probs", None)
+            self.zeroV = j_data.get("zeroV", None)
+            self.emptyPos = j_data.get("emptyPos", None)
+            self.emptyNeg = j_data.get("emptyNeg", None)
+
     def score(self):
         """
         Goes through every review in the test folder and attempts to classify it. 
         Then checks if the classification was right or not and updates the score 
         accordingly. Finally displays the score.
         """
+
+        if (self.probs is None or self.zeroV is None 
+                or self.emptyPos is None or self.emptyNeg is None):
+            print("Generate or import model before running score!")
+            return
+
         gotItRight = 0
         counter = 0
 
@@ -82,6 +125,12 @@ class NaiveBayes:
         attempts to classify a review submited by a user and prints if 
         it is posive or negative
         """
+
+        if (self.probs is None or self.zeroV is None
+                or self.emptyPos is None or self.emptyNeg is None):
+            print("Generate or import model before running classify!")
+            return
+
         review_list = np.array(review.split())
         neg, pos = nb.getProbs(review_list, self.probs, self.zeroV, self.emptyPos, self.emptyNeg)
         if neg > pos:
--- main.py
+++ main.py
@@ -4,43 +4,71 @@ import NaiveBayesFunctions
 from collections import Counter
 import math
 import os
+import sys
 import numpy as np
 import json as json
+
+import tkinter
+from tkinter import filedialog
 
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 class MenuClass:
     def __init__(self, nb):
-        self.state = 0
         self.nb = nb
+        self.clearAllTheThings()
         self.prompt()
 
+    def clearAllTheThings(self):
+        """Clears command prompt in win & ios"""
+        os.system('cls' if os.name == 'nt' else 'clear')
+
     def print_state_menu(self):
+        self.clearAllTheThings()
         with open(os.path.join(__location__,"menus.json"), "r") as data:
             j_data = json.load(data)
-            for line in j_data[self.state]:
+            for line in j_data[0]:
                 print(line)
 
-    def prompt(self, **kwargs):
-        self.print_state_menu()
-        input_v = input("> ")
+    def prompt(self):
+        while True:
+            self.print_state_menu()
+            input_v = input("> ")
 
-        if input_v == "1":
-            self.go_state_1()
-        elif input_v == "2":
-            self.go_state_2()
-        elif input_v == "3":
-            self.go_state_3()
+            if input_v == "generate" or input_v == "1":
+                self.nb.loadData()
 
-    def go_state_1(self):
-        pass
+            elif input_v == "import" or input_v == "2":
+                self.nb.load_data_from_file()
 
-    def go_state_2(self):
-        pass
+            elif input_v == "score" or input_v == "3":
+                self.nb.score()
+                input("Press enter to continue...")
 
-    def go_state_3(self):
-        pass
+            elif input_v == "classify" or input_v == "4":
+                review = input("Give review to classify:")
+                self.nb.classify(review)
+                input("Press enter to continue...")
+
+            elif input_v == "classifyfile" or input_v == "5":
+                print("Select text file to classify")
+                tkinter.Tk().withdraw()
+                file_dir_path = filedialog.askopenfilename()
+                with open(file_dir_path, "r") as data:
+                    self.nb.classify(data.read())
+                input("Press enter to continue...")
+
+            elif input_v == "aboutus" or input_v == "6":
+                input("Press enter to continue...")
+
+            elif input_v == "save" or input_v == "7":
+                self.nb.saveData()
+
+            elif input_v == "exit" or input_v == "8":
+                sys.exit()
+
+            
 
 
 if __name__ == '__main__':
